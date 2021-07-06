aa0b588b77be11956366239540cffab9fb6d9ec3
mahekagarwal <69192437+mahekagarwal@users.noreply.github.com>
2021-07-06 20:36:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+  
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+let empty = document.getElementById('emptylist');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
+empty.addEventListener('click',function(){
+    toDoContainer.remove();
+})
